7bff2e32ed062ead321f0d8c168240fb93310ef0
Jihun Park <pgh979@gmail.com>
2023-11-10 19:16:18 +0900

    add file

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3000; // 사용할 포트 번호
+
+//루트 경로 처리
+app.get('/', (req, res) => {
+  res.send('안녕하세요, Express 서버입니다!');
+});
+
+//추가 경로 처리
+app.get('/home', (req, res) => {
+    res.send('안녕하세요, home 입니다!');
+});
+
+app.get('/html', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
+
+app.post('/login_check', (req, res) => {
+  if(res.body.id == "abc" && res.body.pw == "1234")
+  res.send('로그인 성공');
+  else res.send('로그인 실패');
+});
+
+// 서버 시작
+app.listen(port, () => {
+  console.log('server is running');
+});
